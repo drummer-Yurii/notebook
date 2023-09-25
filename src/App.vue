74baa96afaa4c38cf65e23474a913ededd2374db
Yurii Rybachok <drummer2009@ukr.net>
2023-09-25 20:04:51 +0300

    add global styles & for learning resource

--- src/App.vue
+++ src/App.vue
@@ -39,12 +39,15 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>
--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -8,7 +8,7 @@
     </div>
     <p>{{ description }}</p>
     <nav>
-        <a :href="link"></a>
+        <a :href="link">View Resource</a>
     </nav>
   </li>
 </template>
@@ -20,4 +20,29 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+</style>
