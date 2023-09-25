77f5e25bf9d48394f101e9755376fe326c501431
Yurii Rybachok <drummer2009@ukr.net>
2023-09-25 20:13:07 +0300

    create StoredResource component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <template>
-  <ul>
-    <LearningResource
-      v-for="res in storedResources"
-      :key="res.id"
-      :title="res.title"
-      :description="res.description"
-      :link="res.link"
-    ></LearningResource>
-  </ul>
+  <StoredResource :resource="storedResources"></StoredResource>
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource';
+import StoredResource from './components/learning-resources/StoredResource'
 export default {
   name: 'App',
   components: {
-    LearningResource,
+    StoredResource,
+    
   },
   data() {
     return {
---
+++ src/components/learning-resources/StoredResource.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in resource"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+export default {
+  name: 'StoredResource',
+  components: {
+    LearningResource,
+  },
+  props: ['resource']
+};
+</script>
+
+LearningResource
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    margin: auto;
+    max-width: 40rem;
+}
+</style>
