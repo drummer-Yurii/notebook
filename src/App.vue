a9b062c93184fc48acc201f5a425f60cd604059b
Yurii Rybachok <drummer2009@ukr.net>
2023-09-25 12:41:13 +0300

    add resources component & show him in app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <div></div>
+  <ul>
+    <LearningResource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></LearningResource>
+  </ul>
 </template>
 
 <script>
-
+import LearningResource from './components/learning-resources/LearningResource';
 export default {
   name: 'App',
   components: {
-    
-  }
-}
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-gide',
+          title: 'Official Gide',
+          description: 'The official vue.js Documentation',
+          link: 'https://vue.js.com',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+        <a :href="link"></a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'LearningResource',
+  props: ['title', 'description', 'link']
+};
+</script>
+
+<style scoped></style>
