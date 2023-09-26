ba99a8528242ae27f8410b89c36411fa4c52b387
Yurii Rybachok <drummer2009@ukr.net>
2023-09-26 10:21:37 +0300

    create form for add resource

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,11 +1,56 @@
 <template>
-  <h1>Add Note</h1>
+  <BaseCard>
+    <form>
+        <div class="form-control">
+            <label for="title">Title</label>
+            <input id="title" name="title" type="text" />
+        </div>
+        <div class="form-control">
+            <label for="description">Description</label>
+            <textarea name="description" id="description" rows="3"></textarea>
+        </div>
+        <div class="form-control">
+            <label for="link">Link</label>
+            <input id="link" name="link" type="url" />
+        </div>
+    </form>
+    <div>
+        <BaseButton type="submit">Add Resource</BaseButton>
+    </div>
+  </BaseCard>
 </template>
 
 <script>
+import BaseButton from '../UI/BaseButton'
+import BaseCard from '../UI/BaseCard';
 export default {
+  components: {
+    BaseButton, BaseCard },
   name: 'AddResource',
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+}
+input,
+textarea {
+    display: block;
+    width: 100%;
+    font: inherit;
+    padding: 0.15rem;
+    border: 1px solid #ccc;
+}
+input:focus,
+textarea:focus {
+    outline: none;
+    border-color: #3a0061;
+    background-color: #f7ebff;
+}
+.form-control {
+    margin: 1rem 0;
+}
+</style>
--- src/components/learning-resources/TheResource.vue
+++ src/components/learning-resources/TheResource.vue
@@ -1,7 +1,7 @@
 <template>
   <BaseCard>
-    <BaseButton @click="setSelectedTab('stored-resource')">Stored Notes</BaseButton>
-    <BaseButton @click="setSelectedTab('add-resource')">Add Note</BaseButton>
+    <BaseButton @click="setSelectedTab('stored-resource')" :mode="storedResButtonMode">Stored Notes</BaseButton>
+    <BaseButton @click="setSelectedTab('add-resource')" :mode="addResButtonMode">Add Note</BaseButton>
   </BaseCard>
   <component :is="selectedTab"></component>
 </template>
@@ -43,6 +43,16 @@ export default {
       this.selectedTab = tab;
     },
   },
+
+  computed: {
+    storedResButtonMode() {
+        return this.selectedTab === 'stored-resource' ? null : 'flat'
+    },
+    addResButtonMode() {
+        return this.selectedTab === 'add-resource' ? null : 'flat'
+    }
+  },
+
   provide() {
     return {
         resource: this.storedResources
